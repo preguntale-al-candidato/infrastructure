f3afde682960d241d64400d1ccba05f686d488ab
Julian Nonino <noninojulian@gmail.com>
2023-09-22 21:20:57 +0100

    Add certificate creation

--- prod/terraform.tf
+++ prod/terraform.tf
@@ -10,11 +10,18 @@ terraform {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+      configuration_aliases = [aws.main, aws.acm_provider]
     }
   }
 }
 
 # Configure the AWS Provider
 provider "aws" {
+  alias  = "main"
   region = "eu-west-2"
+}
+
+provider "aws" {
+  alias  = "acm_provider"
+  region = "us-east-1"
 }

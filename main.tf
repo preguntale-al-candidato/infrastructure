958bd38c84f2526d279ceea9e0a2e6a905c60bba
Julian Nonino <noninojulian@gmail.com>
2023-09-29 22:54:19 +0100

    Refactor code

---
+++ main.tf
@@ -0,0 +1,213 @@
+#####
+# VPC
+#####
+resource "aws_vpc" "main" {
+  cidr_block = local.vpc_cidr_block
+}
+
+##################
+# Internet gateway
+##################
+resource "aws_internet_gateway" "internet_gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+################
+# Public subnets
+################
+
+# Subnets
+resource "aws_subnet" "public" {
+  for_each = local.public_subnets
+
+  vpc_id            = aws_vpc.main.id
+  availability_zone = each.value.availability_zone
+  cidr_block        = each.value.cidr_block
+
+  tags = {
+    Name = each.key
+  }
+}
+
+# Route table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Route to access internet
+resource "aws_route" "public_access_to_internet" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.internet_gw.id
+}
+
+# Association of Route Table to Subnets
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.public
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.public.id
+}
+
+# # Elastic IPs for NAT
+# resource "aws_eip" "nat" {
+#   domain = "vpc"
+# }
+
+# # NAT gateways
+# resource "aws_nat_gateway" "nat" {
+#   allocation_id = aws_eip.nat.id
+#   subnet_id     = values(aws_subnet.public)[0].id
+# }
+
+#################
+# Private subnets
+#################
+
+# Subnets
+resource "aws_subnet" "private" {
+  for_each = local.private_subnets
+
+  vpc_id     = aws_vpc.main.id
+  availability_zone = each.value.availability_zone
+  cidr_block = each.value.cidr_block
+
+  tags = {
+    Name = each.key
+  }
+}
+
+# Route table
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+}
+
+# # Route to access internet
+# resource "aws_route" "private_access_to_internet" {
+#   route_table_id         =  aws_route_table.public.id
+#   destination_cidr_block = "0.0.0.0/0"
+#   gateway_id             = aws_nat_gateway.nat.id
+# }
+
+# Association of Route Table to Subnets
+resource "aws_route_table_association" "public" {
+  for_each = aws_subnet.private
+
+  subnet_id      = each.value.id
+  route_table_id = aws_route_table.private.id
+}
+
+#########
+# Route53
+#########
+module "route53" {
+  source  = "cn-terraform/route53/aws"
+  version = "0.0.1"
+
+  create_hosted_zone = true
+  hosted_zone_name   = local.domain_name
+  records            = {}
+}
+
+#################
+# ACM Certificate
+#################
+resource "aws_acm_certificate" "cert" {
+  domain_name               = "*.${local.domain_name}"
+  subject_alternative_names = [local.domain_name]
+  validation_method         = "DNS"
+  tags = {
+    Name = local.domain_name
+  }
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "cert_validation_records" {
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 300
+  type            = each.value.type
+  zone_id         = module.route53.zone_id
+}
+
+resource "aws_acm_certificate_validation" "cert_validation" {
+  # Dependency to guarantee that certificate and DNS records are created before this resource
+  depends_on = [
+    aws_acm_certificate.cert,
+    aws_route53_record.cert_validation_records,
+  ]
+
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation_records : record.fqdn]
+}
+
+################
+# Load Balancing
+################
+# Load Balancer security group
+resource "aws_security_group" "lb_sg" {
+  name        = "${local.name_prefix}-lb"
+  description = "Allow HTTPS to ALB"
+  vpc_id      = aws_vpc.main.id
+}
+
+# LB HTTPS Ingress rule
+resource "aws_security_group_rule" "lb_https_ingress" {
+  security_group_id = aws_security_group.lb_sg.id
+
+  type        = "ingress"
+  from_port   = 443
+  to_port     = 443
+  protocol    = "tcp"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# LB Egress rule
+resource "aws_security_group_rule" "lb_all_egress" {
+  security_group_id = aws_security_group.lb_sg.id
+
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+}
+
+# Application Load Balancer
+resource "aws_lb" "lb" {
+  name               = local.name_prefix
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.lb_sg.id]
+  subnets            = [for subnet in aws_subnet.public : subnet.id]
+
+  enable_deletion_protection = false
+}
+
+# HTTPS ALB listener
+resource "aws_lb_listener" "https" {
+  load_balancer_arn = aws_lb.lb.arn
+  port              = "443"
+  protocol          = "HTTPS"
+  certificate_arn   = aws_acm_certificate.cert.arn
+
+  default_action {
+    type = "fixed-response"
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "Default response not implemented"
+      status_code  = "501"
+    }
+  }
+}

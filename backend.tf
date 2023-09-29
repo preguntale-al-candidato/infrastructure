c19c7d7bc46145d122acae50d216d67d3878ccdb
Julian Nonino <noninojulian@gmail.com>
2023-09-29 10:40:27 +0100

    Add networking

--- backend.tf
+++ backend.tf
@@ -48,3 +48,11 @@ resource "aws_ecr_repository_policy" "backend_deployer_policy" {
   repository = aws_ecr_repository.backend.name
   policy     = data.aws_iam_policy_document.backend_deployer_policy.json
 }
+
+#####################
+# ECS Task Definition
+#####################
+
+#############
+# ECS Service
+#############
--- ecs.tf
+++ ecs.tf
@@ -1,3 +1,40 @@
+#################
+# Base networking
+#################
+module "networking" {
+  source  = "cn-terraform/networking/aws"
+  version = "3.0.0"
+
+  cidr_block = "172.16.0.0/16"
+  single_nat = true
+
+  public_subnets = {
+    first_public_subnet = {
+      availability_zone = "eu-west-2a"
+      cidr_block        = "172.16.0.0/18"
+    }
+    second_public_subnet = {
+      availability_zone = "eu-west-2b"
+      cidr_block        = "172.16.64.0/18"
+    }
+  }
+
+  private_subnets = {
+    first_private_subnet = {
+      availability_zone = "eu-west-2a"
+      cidr_block        = "172.16.128.0/18"
+    }
+    second_private_subnet = {
+      availability_zone = "eu-west-2b"
+      cidr_block        = "172.16.192.0/18"
+    }
+  }
+}
+
+###################
+# Autoscaling group
+###################
+
 #############
 # ECS Cluster
 #############

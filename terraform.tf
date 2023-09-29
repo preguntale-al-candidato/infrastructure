d78aea3b1ec8d792e5d16c8c5f285bc8fa170344
Julian Nonino <noninojulian@gmail.com>
2023-09-29 21:15:46 +0100

    Switch to North Virginia region

--- terraform.tf
+++ terraform.tf
@@ -8,33 +8,18 @@ terraform {
 
   required_providers {
     aws = {
-      source                = "hashicorp/aws"
-      version               = "~> 5.0"
-      configuration_aliases = [aws.acm_provider]
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
     }
   }
 }
 
-# Configure the AWS Main provider
+# Configure the AWS provider
 provider "aws" {
-  region = "eu-west-2"
+  region = "us-east-1"
   default_tags {
     tags = {
       Name        = local.name_prefix
-      Environment = "Prod"
-      Owner       = "craneando.co.uk"
-      Service     = "preguntale-al-candidato"
-    }
-  }
-}
-
-# Configure the AWS ACM provider
-provider "aws" {
-  alias  = "acm_provider"
-  region = "us-east-1"
-
-  default_tags {
-    tags = {
       Environment = "Prod"
       Owner       = "craneando.co.uk"
       Service     = "preguntale-al-candidato"
